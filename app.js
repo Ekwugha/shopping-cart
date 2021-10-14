2213e01d21084ae84c6febad1f6d1430ebc8cfbe
Ekwugha <elloekwugha@gmail.com>
2021-10-14 19:32:42 +0100

    added more packages

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator')
+
 
 // connect to db
 const dbURI = (config.database)
@@ -21,6 +25,54 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan ('dev'));
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+// Express session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+
+// Express validator
+app.use(expressValidator ({
+    errorFormatter: function(param, msg, value) {
+        const namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+
+
+// Express messsages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+
 
 // set routes
 const pages = require('./routes/pages.js')
