4f9bf3b9fe4d179c074fad89a9ce1d7fb2494c12
Ekwugha <elloekwugha@gmail.com>
2021-10-22 11:00:55 +0100

    created product index page

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -7,9 +7,24 @@ const resizeImg = require('resize-img');
 // Get product model
 const Product = require('../models/product')
 
+// Get category model
+const Category = require('../models/category')
+
 // Get products index
 
 router.get('/', (req, res) => {
+    var count;
+
+    Product.count((err, c) => {
+        count = c;
+    });
+
+    Product.find((err, products) => {
+        res.render('admin/products', {
+            products: products,
+            count: count
+        })
+    })
     
 });
 
